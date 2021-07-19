867d8e95bdc3a0ce6a31abf02e1d7b473b04f9f8
di3po <bolatbekd1n@gmail.com>
2021-07-19 14:22:16 -0400

    added migrations and models for categories, products and tags

---
+++ database/factories/Models/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Category;
+use App\Models\Product;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => Category::factory(),
+            'title' => $this->faker->sentence(4),
+            'slug' => $this->faker->slug,
+            'description' => $this->faker->text,
+            'image' => $this->faker->word,
+            'status' => $this->faker->randomElement(["AVAILABLE","NOT"]),
+            'featured' => $this->faker->boolean,
+        ];
+    }
+}
---
+++ database/migrations/2021_07_19_181942_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->string('title');
+            $table->string('slug')->unique()->default('');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->enum('status', ["AVAILABLE","NOT"]);
+            $table->boolean('featured')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
